c303e9628dd241854b1d9bc7e04a8ba8411b29d5
jbroels <jorisb.roels@ugent.be>
2018-12-15 10:42:59 +0100

    Added accuracy related metrics

--- util/metrics.py
+++ util/metrics.py
@@ -26,3 +26,23 @@ def dice(x, y):
     # compute dice
     intersection = np.sum(np.multiply(x, y))
     return 2*(intersection + eps) / (np.sum(x) + np.sum(y) + eps)
+
+# accuracy related metrics:
+#   - accuracy, precision, recall, f-score
+def accuracy_metrics(x, y):
+
+    # binarize
+    x = x > 0.5
+    y = y > 0.5
+
+    tp = np.sum(np.multiply(x, y))
+    tn = np.sum(np.multiply(1-x, 1-y))
+    fp = np.sum(np.multiply(x, 1-y))
+    fn = np.sum(np.multiply(1-x, y))
+
+    accuracy = (tp+tn)/(tp+tn+fp+fn)
+    precision = tp/(tp+fp)
+    recall = tp/(tp+fn)
+    f_score = 2*(precision*recall)/(precision+recall)
+
+    return accuracy, precision, recall, f_score
